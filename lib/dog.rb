a5e3d82d0ad1ecb37a4ec98d94fa8116fc7d7162
Aidan K McBride <aidankmcbride@gmail.com>
2020-03-11 03:56:54 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,8 +1,18 @@
 class Dog 
-  def name 
+  def name          #reads the name of the dog from an instance variable
     @name 
   end 
-  def name=(name)
+  
+  def name=(name)   #writes the name of the dog to an instance variable @name
     @name = name 
   end 
+  
+  def breed=(breed)
+    @breed = breed
+  end 
+  
+  def breed 
+    @breed 
+  end 
 end 
+
